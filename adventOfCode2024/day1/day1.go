e17f4148bd30120f2fa73c126e8146bf36668fe9
canhuynh1998 <canhuynh1998@gmail.com>
2024-12-04 23:07:02 -0800

    Restructured folder and added Part 2 Day 1 AOC2024

--- adventOfCode2024/day1/day1.go
+++ adventOfCode2024/day1/day1.go
@@ -35,18 +35,41 @@ func main() {
 		}
 
 	}
-		slices.Sort(line1)
+	slices.Sort(line1)
 	slices.Sort(line2)
+	part1(line1, line2)
+	part2(line1, line2)
+}
+
+func part1(line1 []int, line2 []int) {
 	distance := 0
 	for idx := 0; idx < len(line1); idx += 1 {
 		distance += abs(line1[idx] - line2[idx])
 	}
-
 	fmt.Println(distance)
 }
 
+func part2(line1 []int, line2 []int) {
+	map12  := make(map[int]int)
+
+	for idx := 0; idx < len(line1); idx += 1 {
+		map12[line1[idx]] = 0
+	}
+
+	for idx := 0; idx < len(line2); idx += 1 {
+		if _, exist := map12[line2[idx]]; exist {
+			map12[line2[idx]] += 1
+		}
+	}
+
+	similar := 0
+	for k, v := range map12 {
+		similar += (k * v)
+	}
+	fmt.Println(similar)
+}
 func abs(x int) int {
-	if x < 0{
+	if x < 0 {
 		return -x
 	}
 	return x
